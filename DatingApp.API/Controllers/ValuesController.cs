383aa4d26bc64e849b796585b2387f880a63cb8a
Stephen Webster <websterinvirginia@hotmail.com>
2020-07-27 15:56:23 -0400

    initial commit

---
+++ DatingApp.API/Controllers/ValuesController.cs
@@ -0,0 +1,93 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DatingApp.API.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private readonly DataContext _context;
+        public ValuesController(DataContext context)
+        {
+            _context = context;
+        }
+
+        // Out of the box
+        // GET api/values
+        // [HttpGet]
+        // public ActionResult<IEnumerable<string>> Get()
+        // {
+        //     //throw new Exception("Test Exception");
+        //     return new string[] { "value1", "value3", "value5" };
+        // }
+
+        // First revision
+        // [HttpGet]
+        // public IActionResult GetValues()
+        // {
+        //     var values = _context.Values.ToList();
+
+        //     return Ok(values);
+        // }
+
+        // Make First revision async
+        [HttpGet]
+        public async Task<IActionResult> GetValues()
+        {
+            var values = await _context.Values.ToListAsync();
+
+            return Ok(values);
+        }
+
+        // GET api/values/5 
+        // [HttpGet("{id}")]
+        // public ActionResult<string> Get(int id)
+        // {
+        //     return "value";
+        // }
+
+        // First revision
+        // GET api/values/5 
+        // [HttpGet("{id}")]
+        // public IActionResult GetValue(int id)
+        // {
+        //     var value = _context.Values.FirstOrDefault(x => x.Id ==id);
+
+        //     return Ok(value);
+        // }
+
+        // Second revsion make async
+        // GET api/values/5 
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetValue(int id)
+        {
+            var value = await _context.Values.FirstOrDefaultAsync(x => x.Id ==id);
+
+            return Ok(value);
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
